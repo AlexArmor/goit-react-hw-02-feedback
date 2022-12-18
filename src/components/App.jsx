e238f568edc68aa5235790d70fd6ba08e19f7a91
Oleksandr Bron <lexlutermotion@gmail.com>
2022-12-18 21:07:12 +0200

    contol commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+import { Section } from './Section';
+import { Notification } from './Notification';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = buttonName => {
+    this.setState(prevState => ({
+      [buttonName]: prevState[buttonName] + 1,
+    }));
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    return (
+      <>
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <p>Good:{this.state.good}</p>
+            <p>Neutral:{this.state.neutral}</p>
+            <p>Bad:{this.state.bad}</p>
+            <p>Positive feedback:</p>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
